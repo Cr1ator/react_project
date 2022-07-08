5c2f7933ba8a5719fe825f4aa5c9ca3ccf6bcfd8
Amid <azorty71@gmail.com>
2022-07-08 15:32:32 +0300

    Broken into components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
-
-const inputClick = () => console.log("Clicked")
-const mouseOver = () => console.log("Mouse Over")
-
-const helpText = "Help text"
-
-function App() {
-  return (<div className='name'>
-  <h1>{helpText}</h1>
-  <input placeholder={helpText}
-      onClick={inputClick} onMouseEnter={mouseOver} />
-  <p>{helpText === "Help text!" ? "Yes" : "No"}</p>
-</div>)
-}
+import App from './App'
+import './css/main.css'
 
 const app = ReactDOMClient.createRoot(document.getElementById("app"))
 
