3ce372e7b093c7802b342cee6469ac92ed0b34cf
Amid <azorty71@gmail.com>
2022-07-08 17:50:04 +0300

    add button `add suer`

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import Header from './components/Header';
+import Users from './components/Users';
+import AddUser from './components/AddUser';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users : [
+                {
+                    id: 1, 
+                    firstname: 'Bob', 
+                    lastname: 'Marley', 
+                    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sed quos, fugiat aliquid error culpa.',
+                    isHappy: true
+                },
+                {
+                    id: 2, 
+                    firstname: 'John', 
+                    lastname: 'Doe', 
+                    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sed quos, fugiat aliquid error culpa.', 
+                    isHappy: false
+                }
+            ]
+        }
+        this.addUser = this.addUser.bind(this)
+    }
     render() {
         return (<div>
         <Header title="Список пользователей"/>
+        <main>
+            <Users users={this.state.users}/>
+        </main>
+        <aside>
+            <AddUser onAdd={this.addUser}/>
+        </aside>
     </div>)
+    }
+    addUser(user) {
+        const id = this.state.users.length + 1
+        this.setState({ users: [...this.state.users, {id, ...user}] })
     }
 }
 
