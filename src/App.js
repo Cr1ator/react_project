e76ffa7a6ef24e3ca352066528c9d5106b7d480e
Amid <azorty71@gmail.com>
2022-07-08 16:10:30 +0300

    change project structure

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
-import Image from './components/Image';
-import logo from "./img/logo.jpg"
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            helpText: "Help text",
-            userData: ""
-        }
-        this.inputClick = this.inputClick.bind(this)
-    }
-
-    componentDidUpdate(prevProp) {
-        if(this.state.helpText !== "Help")
-            console.log("Some")
-    }
-
-    helpText = "Help text"
     render() {
-        return (<div className='name'>
-        <Header title="Шапка сайта"/>
-        <h1>{this.state.helpText}</h1>
-        <h2>{this.state.userData}</h2>
-        <input placeholder={this.state.helpText}
-            onChange={event => this.setState({userData: event.target.value })}
-            onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-        <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
-        <Image image={logo}/>
+        return (<div>
+        <Header title="Список пользователей"/>
     </div>)
     }
-
-    inputClick() {
-        this.setState({helpText: "Changed"})
-    }
-    mouseOver() {console.log("mouseOver")}
 }
 
 export default App
