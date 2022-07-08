26d4a064c43df9084d0fec69ab71d364da374565
Amid <azorty71@gmail.com>
2022-07-08 15:45:42 +0300

    add state

--- src/App.js
+++ src/App.js
@@ -4,19 +4,33 @@ import Image from './components/Image';
 import logo from "./img/logo.jpg"
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            helpText: "Help text",
+            userData: ""
+        }
+        this.inputClick = this.inputClick.bind(this)
+    }
+
     helpText = "Help text"
     render() {
         return (<div className='name'>
         <Header title="Шапка сайта"/>
-        <h1>{this.helpText}</h1>
-        <input placeholder={this.helpText}
+        <h1>{this.state.helpText}</h1>
+        <h2>{this.state.userData}</h2>
+        <input placeholder={this.state.helpText}
+            onChange={event => this.setState({userData: event.target.value })}
             onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-        <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
+        <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
         <Image image={logo}/>
     </div>)
     }
 
-    inputClick() {console.log("click")}
+    inputClick() {
+        this.setState({helpText: "Changed"})
+        console.log("click")
+    }
     mouseOver() {console.log("mouseOver")}
 }
 
